6777ab2caab6d0bfb120441657e14aabf9701649
Lundin Matthews <lundinmatthews@me.com>
2018-04-09 21:02:26 -0500

    es6 import working
    using es6 import

--- app.js
+++ app.js
@@ -1,11 +1,46 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+import express from 'express'
+import bodyParser from 'body-parser'
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express'
+import { makeExecutableSchema } from 'graphql-tools'
 
 require('dotenv').config()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5007
+
+// Some fake data
+const books = [
+  {
+    title: "Harry Potter and the Sorcerer's stone",
+    author: 'J.K. Rowling'
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton'
+  }
+]
+
+// The GraphQL schema in string form
+const typeDefs = `
+  type Query { books: [Book] }
+  type Book { title: String, author: String }
+`
+
+// The resolvers
+const resolvers = {
+  Query: { books: () => books }
+}
+
+// Put together a schema
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+})
 
 const app = express()
 app.use(bodyParser.json())
+// The GraphQL endpoint
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }))
+// GraphiQL, a visual editor for queries
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
 
 app.get('/', (req, res) => {
   res.send('WE ARE LIVE')
