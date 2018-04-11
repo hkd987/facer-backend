48595427536c0e39cf872e944d609b024cf63976
hkd987 <lundinmatthews@me.com>
2018-04-11 10:10:24 -0500

    get a users todos
    when you pass in a token you will get your todos!!

--- src/resolvers/resolvers/tasksResolver.js
+++ src/resolvers/resolvers/tasksResolver.js
@@ -1,4 +1,12 @@
 export default {
+  Query: {
+    allTasks: (parent, args, { models, token }) => {
+      return models.Task.findAll({ where: { userId: token.id } })
+    },
+    getTask: (parent, { id }, { models }) => {
+      return models.Task.findOne({ where: { id: id } })
+    }
+  },
   Mutation: {
     createTask: (parent, args, { models, token }) => {
       return models.Task.create({...args, userId: token.id})
