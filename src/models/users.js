ac62cae12f801a0e7d9606c3b8a615e25427753e
Lundin Matthews <lundinmatthews@me.com>
2018-04-11 20:13:02 -0500

    validate username
    need to make sure user name is username clean

--- src/models/users.js
+++ src/models/users.js
@@ -2,7 +2,17 @@ export default (sequelize, DataTypes) => {
   const User = sequelize.define('user', {
     username: {
       type: DataTypes.STRING,
-      unique: true
+      unique: true,
+      validate: {
+        isAlphanumeric: {
+          args: true,
+          msg: 'The username can only contain letters and numbers'
+        },
+        len: {
+          args: [6, 33],
+          msg: 'The username needs to be between 6 and 33 characters long'
+        }
+      }
     },
     password: DataTypes.STRING
   })
